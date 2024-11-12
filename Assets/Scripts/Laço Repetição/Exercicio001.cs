ae9d2325fc9f5a0074f19cd8cee03cfa9417c66d
EllenFerreira18 <ellen.github@gmail.com>
2024-11-12 15:09:22 -0300

    feat: Exercicio001

---
+++ Assets/Scripts/Laço Repetição/Exercicio001.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Exercicio001 : MonoBehaviour
+{
+    void Start()
+    {
+        for (int i = 5; i > 0; i--)
+        {
+            print(i);
+        }
+        print("Início da partida!");
+
+    }
+
+    void Update()
+    {
+        
+    }
+}
